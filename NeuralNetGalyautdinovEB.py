6d5af2d35b3f43b469ebbdd1251d2f5e9498b328
VoganRussia <r3-d2@mail.ru>
2022-10-13 16:16:49 +0300

    Добавьте файлы проекта.

---
+++ NeuralNetGalyautdinovEB.py
@@ -0,0 +1,17 @@
+
+
+from configs.config import CFG
+from model.ExampleModel import ExampleModel
+
+
+def run():
+    """Builds model, loads data, trains and evaluates"""
+    model = ExampleModel(CFG)
+    model.load_data()
+    model.build()
+    model.train()
+    model.evaluate()
+
+
+if __name__ == '__main__':
+    run()
